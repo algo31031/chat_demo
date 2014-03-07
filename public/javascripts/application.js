d0b8acf750697a7c475deb3a9176c0501824c477
han.bing <han.bing@e-trust.com.cn>
2014-03-07 15:42:16 +0800

    将localhost替换掉,否则其他机器访问不好用

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-var faye = new Faye.Client('http://localhost:9292/faye');
+// var faye = new Faye.Client('http://192.168.10:60:9292/faye');
 
-faye.subscribe('/posts/new', function (data) {
-  $("posts_list").insert({top: data.toString()});
-  new Effect.Highlight($("posts_list").childElements()[0].childElements()[0].id);
-});
+// faye.subscribe('/posts/new', function (data) {
+//   $("posts_list").insert({top: data.toString()});
+//   new Effect.Highlight($("posts_list").childElements()[0].childElements()[0].id);
+// });
