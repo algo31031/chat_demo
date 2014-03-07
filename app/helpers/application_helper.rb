a22f0bb377930a4fbe7c66839a5233baa1fc3ffb
han.bing <han.bing@e-trust.com.cn>
2014-03-07 14:15:34 +0800

    基本可用

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def broadcast(channel, &block)
+    message = {:channel => channel, :data => capture(&block)}
+    uri = URI.parse("http://localhost:9292/faye")
+    Net::HTTP.post_form(uri, :message => message.to_json)
+  end
+
+
 end
